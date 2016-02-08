023f754165966313faa9269dfc4921954912f681
Moisés Ackerman <unacson@gmail.com>
2016-02-08 18:09:38 -0430

    added Parse, logo, FAB, etc.

--- app/src/main/java/ar/recicl/reciclar/activity/Feed.java
+++ app/src/main/java/ar/recicl/reciclar/activity/Feed.java
@@ -1,16 +1,33 @@
-package ar.recicl.reciclar;
+package ar.recicl.reciclar.activity;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class InitActivity extends ActionBarActivity {
+import ar.recicl.reciclar.R;
+
+public class Feed extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_init);
+        setContentView(R.layout.activity_feed);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
     @Override
