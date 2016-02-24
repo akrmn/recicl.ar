1c2cd7e8d0ab62110df04ee34ccad718610767f5
Carlos <carlosrmd94@gmail.com>
2016-02-23 22:13:32 -0430

    Login activity ready

--- app/src/main/java/ar/recicl/reciclar/activity/Login.java
+++ app/src/main/java/ar/recicl/reciclar/activity/Login.java
@@ -1,5 +1,6 @@
 package ar.recicl.reciclar.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.widget.EditText;
@@ -11,15 +12,12 @@ import butterknife.OnClick;
 public class Login extends Base {
 
     @Bind(R.id.input_email) EditText user_email;
-    @Bind(R.id.input_password1) EditText user_password1;
-    @Bind(R.id.input_password2) EditText user_password2;
+    @Bind(R.id.input_password) EditText user_password;
 
     @Bind(R.id.emailWrapper)
     TextInputLayout wrapper_email;
     @Bind(R.id.password1Wrapper)
-    TextInputLayout wrapper_pass1;
-    @Bind(R.id.password2Wrapper)
-    TextInputLayout wrapper_pass2;
+    TextInputLayout wrapper_pass;
 
 
     public Login() {
@@ -31,30 +29,25 @@ public class Login extends Base {
         super.onCreate(savedInstanceState);
     }
 
-    @OnClick(R.id.button_complete_register)
-    void onButtonCompleteRegisterClick() {
+    @OnClick(R.id.button_complete_login)
+    void onButtonCompleteLoginClick() {
         boolean error = false;
         wrapper_email.setError(null);
-        wrapper_pass1.setError(null);
-        wrapper_pass2.setError(null);
+        wrapper_pass.setError(null);
+
         if(getTextAsString(user_email).equals("")){
             wrapper_email.setError(getString(R.string.required_field_error));
             error = true;
         }
-        if(getTextAsString(user_password1).equals("")){
-            wrapper_pass1.setError(getString(R.string.required_field_error));
-            error = true;
-        }
-        if(getTextAsString(user_password2).equals("")){
-            wrapper_pass2.setError(getString(R.string.required_field_error));
-            error = true;
-        }
-        if(!getTextAsString(user_password1).equals(getTextAsString(user_password2))){
-            wrapper_pass2.setError(getString(R.string.match_pass_error));
+        if(getTextAsString(user_password).equals("")){
+            wrapper_pass.setError(getString(R.string.required_field_error));
             error = true;
         }
         if (error){
             return;
         }
+        Intent intent = new Intent(this, Feed.class);
+        startActivity(intent);
+        finish();
     }
 }
